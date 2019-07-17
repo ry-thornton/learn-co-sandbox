1397393405b2fac30b27f64eeeeb047d466b9b60
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-17 15:48:15 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,12 @@
+# run_code_inside = true
+# puts "Code before if...end"
+# if run_code_inside
+  # puts "code inside"
+# end
+# puts "Code after if...end"
+
+count = 0
+while count < 3 do
+  puts "I am the #{count}, I love to count!"
+  count = count + 1
+end
